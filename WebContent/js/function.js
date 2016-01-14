8d53292d46ab896e496154d4aa319114bf2187ee
Gaoxiang Zhang <Gaoxiang Zhang@DESKTOP-7N8O26B>
2016-01-14 23:12:26 +0800

    2016.1.14 new version of website, LingPipe example

--- WebContent/js/function.js
+++ WebContent/js/function.js
@@ -1,2 +1,39 @@
-function myFunction(){
+function loadXMLDoc(url){
+	xmlhttp = null;
+	if(window.XMLHttpRequest){
+		xmlhttp = new XMLHttpRequest();
+	} else if (window.ActiveXObject){
+		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	if(xmlhttp != null){
+		xmlhttp.onreadystatechange = state_Change;
+		xmlhttp.open("GET", url, true);
+		xmlhttp.send(null);
+	} else {
+		alert("Your browser does not support XMLHTTP");
+	}
+}
+function state_Change(){
+	if(xmlhttp.readyState == 4){
+		if (xmlhttp.status == 200){
+			document.getElementById('T1').innerHTML = xmlhttp.responseText;
+			var r = $('<input type="button" value="Hello"/>')
+			$("body").append(r);
+		} else {
+			alert("Problem retrieving data:" + xmlhttp.statusText);
+		}
+	}
+}
+
+function sendRequest(){
+	var xmlhttp;
+	if(window.XMLHttpRequest){
+		xmlhttp = new XMLHttpRequest();
+	} else{
+		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	var content = documents.getElementById("text_container");
+	var selected = documents.getElementById("nbest");
+	
+	alert(content + selected);
 }
